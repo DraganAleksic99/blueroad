a55a3c005b2fb38a86c7cbd4b09d8512a3c73fff
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-01-08 16:08:52 +0100

    Add create post and post photo features
    -Update post.routes.ts
    -Update post.controller.ts

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,6 +1,8 @@
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
+import fs from 'fs'
 import Post from '../models/post.model'
 import dbErrorHandler from '../utils/dbErrorHandler'
+import formidable from 'formidable'
 
 const listNewsFeed = async (req: Request, res: Response) => {
   const following = req.profile.following
@@ -34,7 +36,57 @@ const listByUser = async (req: Request, res: Response) => {
   }
 }
 
+const create = (req: Request, res: Response) => {
+  const form = formidable({ keepExtensions: true })
+
+  form.parse(req, async (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({ error: 'Image could not be uploaded' })
+    }
+    const post = new Post({ text: fields.text?.toString() || '' })
+    post.postedBy = req.profile
+
+    if (files.photo) {
+      post.photo.data = fs.readFileSync(files.photo[0].filepath)
+      post.photo.contentType = files.photo[0].mimetype
+    }
+    try {
+      const result = await post.save()
+      res.json(result)
+    } catch (err) {
+      return res.status(400).json({
+        error: dbErrorHandler.getErrorMessage(err)
+      })
+    }
+  })
+}
+
+const photo = (req: Request, res: Response) => {
+  res.set('Cross-Origin-Resource-Policy', 'false')
+  res.set('Content-Type', req.post.photo.contentType)
+  return res.send(req.post.photo.data)
+}
+
+const postById = async (req: Request, res: Response, next: NextFunction, id: string) => {
+  try {
+    const post = await Post.findById(id).populate('postedBy', '_id name').exec()
+    if (!post)
+      return res.status(400).json({
+        error: 'Post not found'
+      })
+    req.post = post
+    next()
+  } catch (err) {
+    return res.status(400).json({
+      error: 'Could not retrieve the post'
+    })
+  }
+}
+
 export default {
   listNewsFeed,
-  listByUser
+  listByUser,
+  create,
+  photo,
+  postById
 }
--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -7,7 +7,10 @@ const router = Router()
 
 router.route('/api/posts/feed/:userId').get(authCtrl.requireSignIn, postCtrl.listNewsFeed)
 router.route('/api/post/by/:userId').get(authCtrl.requireSignIn, postCtrl.listByUser)
+router.route('/api/posts/new/:userId').post(authCtrl.requireSignIn, postCtrl.create)
+router.route('/api/posts/photo/:postId').get(postCtrl.photo)
 
 router.param('userId', userCtrl.userById)
+router.param('postId', postCtrl.postById)
 
 export default router
