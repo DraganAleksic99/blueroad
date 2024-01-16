208ed7a2fa6e85698811389f5d4b373667a5b856
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-01-16 15:19:02 +0100

    Add add new post feature
    -Create Post.tsx component
    -Update Profile.tsx component

---
+++ src/views/post/NewPost.tsx
@@ -0,0 +1,118 @@
+import { useState, useEffect } from 'react'
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Button,
+  TextField,
+  Typography,
+  Avatar,
+  Icon,
+  IconButton,
+  useTheme
+} from '@mui/material'
+import { PhotoCamera } from '@mui/icons-material'
+import { createPost } from '../../services/postService'
+import auth from '../../auth/authHelper'
+import { User } from '../Profile'
+
+const baseUrl = 'https://social-media-app-e2ia.onrender.com'
+
+export default function NewPost({ addPost }) {
+  const theme = useTheme()
+  const [values, setValues] = useState({
+    text: '',
+    photo: null,
+    error: ''
+  })
+  const [user, setUser] = useState<User | Record<string, never>>({})
+  const jwt = auth.isAuthenticated()
+
+  useEffect(() => {
+    setUser(auth.isAuthenticated().user)
+  }, [])
+
+  const clickPost = () => {
+    const postData = new FormData()
+    values.text && postData.append('text', values.text)
+    values.photo && postData.append('photo', values.photo)
+
+    createPost(
+      {
+        userId: jwt.user._id
+      },
+      {
+        t: jwt.token
+      },
+      postData
+    ).then(data => {
+      if (data.error) {
+        setValues({ ...values, error: data.error })
+      } else {
+        setValues({ ...values, text: '', photo: null })
+        addPost(data)
+      }
+    })
+  }
+
+  const handleChange = (name: string) => event => {
+    const value = name === 'photo' ? event.target.files[0] : event.target.value
+    setValues({ ...values, [name]: value })
+  }
+
+  return (
+    <div>
+      <Card>
+        <CardHeader
+          sx={{ paddingBlockEnd: 0 }}
+          avatar={<Avatar src={baseUrl + '/api/users/photo/' + user._id} />}
+          title={user.name}
+        />
+        <CardContent sx={{ ml: theme.spacing(7), paddingBlockStart: 0 }}>
+          <TextField
+            placeholder="Share your thoughts ..."
+            multiline
+            rows="3"
+            value={values.text}
+            onChange={handleChange('text')}
+            margin="normal"
+          />
+          <input
+            accept="image/*"
+            onChange={handleChange('photo')}
+            id="icon-button-file"
+            type="file"
+            style={{ display: 'none' }}
+          />
+          <label htmlFor="icon-button-file">
+            <IconButton
+              color="secondary"
+              component="span"
+              sx={{ marginInlineStart: theme.spacing(2) }}
+            >
+              <PhotoCamera />
+            </IconButton>
+          </label>{' '}
+          <span>{values.photo ? values.photo.name : ''}</span>
+          {values.error && (
+            <Typography component="p" color="error">
+              <Icon color="error">error</Icon>
+              {values.error}
+            </Typography>
+          )}
+        </CardContent>
+        <CardActions sx={{ ml: theme.spacing(7), pl: theme.spacing(2), mb: theme.spacing(1) }}>
+          <Button
+            color="primary"
+            variant="contained"
+            disabled={values.text === ''}
+            onClick={clickPost}
+          >
+            POST
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  )
+}
