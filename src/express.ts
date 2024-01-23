1da67e3b825448e7023dd719f4fd05e09691daf5
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-01-23 22:02:25 +0100

    Fix: cors issue

--- src/express.ts
+++ src/express.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response, NextFunction } from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compress from 'compression'
+import cors from 'cors'
 import helmet from 'helmet'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
@@ -14,6 +15,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
+app.use(
+  cors({
+    credentials: true
+  })
+)
 
 app.use('/', userRoutes)
 app.use('/', authRoutes)
