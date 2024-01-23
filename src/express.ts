c4312c44d3c7c6084899e2620900701a07cf3e3b
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-01-23 21:44:02 +0100

    Fix: remove cors

--- src/express.ts
+++ src/express.ts
@@ -2,7 +2,6 @@ import express, { Express, Request, Response, NextFunction } from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compress from 'compression'
-import cors from 'cors'
 import helmet from 'helmet'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
@@ -15,11 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
-app.use(
-  cors({
-    credentials: true
-  })
-)
 
 app.use('/', userRoutes)
 app.use('/', authRoutes)
