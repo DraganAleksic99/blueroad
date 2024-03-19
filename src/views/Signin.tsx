1189ce4f921c47d4d627dcd77f8f372d1d041488
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-03-19 19:29:19 +0100

    update Signin component

--- src/views/Signin.tsx
+++ src/views/Signin.tsx
@@ -1,19 +1,30 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { signin } from '../auth/apiAuth'
 import auth from '../auth/authHelper'
 import { Navigate } from 'react-router'
-import { Card, CardContent, Typography, TextField, CardActions, Button } from '@mui/material'
+import {
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+  CardActions,
+  Button,
+  Box,
+  Alert
+} from '@mui/material'
 
 export default function Signin() {
   const [values, setValues] = useState({
-    email: '',
-    password: '',
+    email: 'demo@gmail.com',
+    password: 'Pass123',
     error: '',
     redirectToRefferer: false
   })
-  const handleChange = name => event => {
+
+  const handleChange = (name: string) => (event: ChangeEvent<HTMLInputElement>) => {
     setValues({ ...values, [name]: event.target.value })
   }
+
   const clickSubmit = () => {
     const user = {
       email: values.email || undefined,
@@ -29,10 +40,13 @@ export default function Signin() {
       }
     })
   }
+
   const { redirectToRefferer } = values
+
   if (redirectToRefferer) {
     return <Navigate to="/" />
   }
+
   return (
     <div
       style={{ height: '60vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
@@ -42,6 +56,7 @@ export default function Signin() {
           <Typography variant="h6">Sign In</Typography>
           <TextField
             id="email"
+            type="email"
             label="Email"
             value={values.email}
             margin="normal"
@@ -50,6 +65,7 @@ export default function Signin() {
           <br />
           <TextField
             id="password"
+            type="password"
             label="Password"
             value={values.password}
             margin="normal"
@@ -67,6 +83,14 @@ export default function Signin() {
             Submit
           </Button>
         </CardActions>
+        <Box mt={2}>
+          <Alert severity="info">
+            <div>
+              Use <b>demo@gmail.com</b>
+              <br></br> and password <b>Pass123!</b>
+            </div>
+          </Alert>
+        </Box>
       </Card>
     </div>
   )
