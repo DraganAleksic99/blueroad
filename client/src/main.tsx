df7f33e232767f4b42212ca663fc2cd91ea79b5d
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-11-11 19:39:17 +0100

    update (client): update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
 import ReactDOM from 'react-dom/client'
-import { Route, createRoutesFromElements, createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  Route,
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import './index.css'
+
 import Layout from './App'
 import ProtectedRoute from './components/ProtectedRoute'
-import Users, { usersLoader} from './routes/Users'
+import Users, { usersLoader } from './routes/Users'
 import Signin from './views/Signin'
 import Signup from './views/Signup'
 import Profile from './views/Profile'
@@ -16,13 +23,7 @@ const queryClient = new QueryClient()
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route
-      element={
-        <QueryClientProvider client={queryClient}>
-          <Layout />
-        </QueryClientProvider>
-      }
-    >
+    <Route element={<Layout />}>
       <Route path="/">
         <Route
           index
@@ -65,5 +66,7 @@ const router = createBrowserRouter(
 )
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
 )
