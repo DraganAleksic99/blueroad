dc1eabf6ce85a16de77cd70b79d02be56622b466
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-11-16 23:30:32 +0100

    fix (client): use user id as key

--- client/src/components/FollowGrid.tsx
+++ client/src/components/FollowGrid.tsx
@@ -6,9 +6,9 @@ export default function FollowGrid({ users = [] }: { users: TUser[] }) {
   return (
     <div>
       <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2 }}>
-        {users.map((user, i) => {
+        {users.map((user) => {
           return (
-            <Box sx={{ position: 'relative', flex: '0 0 32.31%' }} key={i}>
+            <Box sx={{ position: 'relative', flex: '0 0 32.31%' }} key={user._id}>
               <GridCard user={user} />
             </Box>
           )
