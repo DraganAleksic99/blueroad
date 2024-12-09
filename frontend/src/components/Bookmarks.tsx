56f1925738bb77b7287ff8511c8b4cc3ee0cee10
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-12-10 00:30:02 +0100

    update (client): add SectionTitle component

--- frontend/src/components/Bookmarks.tsx
+++ frontend/src/components/Bookmarks.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
+import { Box } from '@mui/material'
 import PostList from '../views/post/PostList'
+import SectionTitle from './SectionTitle'
 import { getBookmarks } from '../services/userService'
 import auth, { Session } from '../auth/authHelper'
-import { Box } from '@mui/material'
 
 export default function Bookmarks() {
   const { user, token }: Session = auth.isAuthenticated()
@@ -16,6 +17,7 @@ export default function Bookmarks() {
 
   return (
     <Box sx={{ borderRight: '1px solid #e5e7eb' }}>
+      <SectionTitle title="Bookmarks" />
       <PostList posts={data?.bookmarkedPosts} arePostsPending={isPending} />
     </Box>
   )
